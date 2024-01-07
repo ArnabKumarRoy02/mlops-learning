24f442957ed01728618264cd6db89e528f399b1e
Arnab Kumar Roy <arnabroy770@gmail.com>
2024-01-08 00:20:42 +0530

    updated the pipeline for training

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -7,6 +7,6 @@ from steps.evaluate_model import eval_model
 @pipeline(enable_cache=True)
 def train_pipeline(data_path: str):
     df = ingest_df(data_path)
-    clean_df(df)
-    train_model(df)
-    eval_model(df)
+    X_train, X_test, y_train, y_test = clean_df(df)
+    model = train_model(X_train, X_test, y_train, y_test)
+    r2_score, rmse = eval_model(model, X_test, y_test)
